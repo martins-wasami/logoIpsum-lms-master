f393a9e6144eaf122ff6938ee458292f6db69f54
Martins Wasami <wasamimartin@gmail.com>
2024-01-26 11:40:59 +0100

    modified sidebar

---
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -0,0 +1,30 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { DataTable } from "./_components/DataTable";
+import { columns } from "./_components/Columns";
+
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return ( 
+    <div className="p-6">
+      <DataTable columns={columns} data={courses} />
+    </div>
+   );
+}
+ 
+export default CoursesPage;
